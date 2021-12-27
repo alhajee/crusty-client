6ea6e349690caea873bf6fda4e1dbdde590db5e6
Muhammad Alhajee <alhajee2009@gmail.com>
2021-12-27 13:08:40 +0100

    create routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { NavBar } from '../components'
+import { GroceriesList, GroceriesInsert, GroceriesUpdate } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,6 +10,15 @@ function App() {
     return (
         <Router>
             <NavBar />
+            <Switch>
+                <Route path="/groceries/list" exact component={GroceriesList} />
+                <Route path="/groceries/create" exact component={GroceriesInsert} />
+                <Route
+                    path="/groceries/update/:id"
+                    exact
+                    component={GroceriesUpdate}
+                />
+            </Switch>
         </Router>
     )
 }
